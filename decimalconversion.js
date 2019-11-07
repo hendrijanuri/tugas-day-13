a3256cd4bffef3ed4b31ac721cf07e50bcfa5cde
hendrijanuri <hendrijanuri@gmail.com>
2019-11-07 22:46:53 +0700

    EUREKA

--- decimalconversion.js
+++ decimalconversion.js
@@ -8,9 +8,30 @@ var desimalKeBiner=(a)=> {
     return biner
 }
 
+var desimalKeHex=(a)=>{
+    let output = []
+    while (a>=1) {
+        if (a%16==10) {
+            output.push('A')
+        }else if (a%16==11) {
+            output.push('B')
+        }else if (a%16==12) {
+            output.push('C')
+        }else if (a%16==13) {
+            output.push('D')
+        }else if (a%16==14) {
+            output.push('E')
+        }else if (a%16==15) {
+            output.push('F')
+        }else {
+            output.push(a % 16)   
+        }
+        a = Math.floor(a / 16)
+    }
+    let biner=output.reverse().join('')
+    return biner
+}
 
-// var desimalKeHex=()=>{
-
-
-
-console.log(desimalKeBiner(1000))
+var number=1000
+console.log(desimalKeBiner(number))
+console.log(desimalKeHex(number))
