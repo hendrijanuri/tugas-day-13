ff5fef0b42dd6a93fae55e5eef79d886aea6845b
hendrijanuri <hendrijanuri@gmail.com>
2019-11-07 22:19:15 +0700

    stuck

--- decimalconversion.js
+++ decimalconversion.js
@@ -1,3 +1,16 @@
-var desimalKeBiner=()=> {
-    
+var desimalKeBiner=(a)=> {
+    let output = []
+    while (a>=1) {
+        output.push(a % 2) 
+        a /= 2            
+    }
+    // let biner=output.reverse().join('')
+    return output
 }
+
+
+var desimalKeHex=()=>{
+
+}
+
+console.log(desimalKeBiner(5))
