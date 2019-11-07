aa27568d4b54defac4f892b9206e97648e72b6e9
hendrijanuri <hendrijanuri@gmail.com>
2019-11-07 23:48:10 +0700

    done

---
+++ date.js
@@ -0,0 +1,20 @@
+var tanggal = 12012012
+console.log(tanggal+' >>> '+tanggal(tanggal))
+
+var tanggal=(str)=>{
+    var arr=str.toString().split('')
+    console.log(arr)
+    var dates=arr.slice(0,2)
+    dates=dates.join('')
+    
+    var month=arr.slice(2,4)
+    month=month.join('')
+    var intMonth=parseInt(month)
+    var arrayMonths=['Jan','Feb','Mar','Apr','Mei','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+    var months=arrayMonths[intMonth-1]
+    
+    var years=arr.slice(4,8)
+    years=years.join('')
+    
+    return console.log(dates+'-'+months+'-'+years)
+}
